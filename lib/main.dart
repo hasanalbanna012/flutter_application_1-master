b4d0858ae75759a012c07ab84c6620edefb104a5
hasanalbanna012 <banna15-5064@diu.edu.bd>
2025-02-19 12:51:15 +0600

    task4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyWidget());
+  runApp(MyWidget());
 }
 
 class MyWidget extends StatelessWidget {
@@ -13,22 +13,63 @@ class MyWidget extends StatelessWidget {
       title: "Flutter project",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Star Widget"),
+          title: Text("Star Widget"),
         ),
-        body:Container(height: double.infinity,width:double.infinity,
-         child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: const Color.fromARGB(255, 230, 223, 223),
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center, 
-          children: List.generate(100, (index)=>Icon(
-              Icons.radar,
-              color: Colors.yellow,
-              size: index.toDouble(),
-            ),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    child: Text("50% off"),
+                    decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(20.0),
+                      color: Colors.white, 
+          )
+                  ),
+                  Align( 
+                    alignment: Alignment.topRight
+                    Icon(
+                    Icons.favorite_border_outlined,
+                    color: Colors.grey,
+                    size: 50.0,
+                    
+                  ),),
+                ],
+              ),
+              Image(
+                image: AssetImage('assests/images.jpg'),
+              ),
+              Text(
+                "Nike air shoes",
+                textAlign: TextAlign.center,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "500 taka",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    "550 taka",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  ),),);
+    );
   }
 }
