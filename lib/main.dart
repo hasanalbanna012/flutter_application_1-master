ae8e8b53f8d488e219c7d8d90783680a98cebc1d
hasanalbanna012 <banna15-5064@diu.edu.bd>
2025-02-19 11:50:30 +0600

    task3

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter project",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Star Widget"),
+        ),
+        body:Container(height: double.infinity,width:double.infinity,
+         child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          children: List.generate(100, (index)=>Icon(
+              Icons.radar,
+              color: Colors.yellow,
+              size: index.toDouble(),
+            ),
+        ),
+      ),
+    ),
+  ),),);
+  }
+}
